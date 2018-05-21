a90bfbcd15c7ff73d9459f059cf87ea98c1c57a1
atans2468 <austintans7@gmail.com>
2018-05-21 14:18:27 -0700

    adding in QR page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, ActionSheetController } from 'ionic-angular';
+
+import { ItemReviewPage } from '../item-review/item-review';
+
+import { Items } from '../../providers/providers';
+
+import { Item } from '../../models/item';
+
 
 @Component({
   selector: 'page-profile',
@@ -7,8 +14,12 @@ import { NavController } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController) {
+  //retrieve data from items.ts carried over from list-master
+  item: any;
+  //data retrieved
 
+  constructor(public navCtrl: NavController, navParams: NavParams, items: Items, public actionSheetCtrl: ActionSheetController) {
+    this.item = navParams.get('item') || items.defaultItem;
   }
 
 }
